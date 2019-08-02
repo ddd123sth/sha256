09c784489eee8154f9686bc48589fb661532a9de
unknown <18300577079@163.com>
2019-08-02 16:30:33 +0800

    这是canvas练习

---
+++ Canvas/Controllers/CanvasController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Canvas.Controllers
+{
+    public class CanvasController : Controller
+    {
+        // GET: Canvas
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Canvas_View()
+        {
+            return View();
+        }
+    }
+}
